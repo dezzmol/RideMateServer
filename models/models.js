09fe98b53745256c912af8d670c0df537e9422cd
Radmir Musabaev <musabaevradmir@gmail.com>
2023-09-29 19:09:51 +0400

    добавил поле с именем в модель

--- models/models.js
+++ models/models.js
@@ -4,6 +4,7 @@ const {DataTypes} = require('sequelize')
 const User = sequelize.define('user', {
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
     email: {type: DataTypes.STRING, allowNull: false, unique: true},
+    name: {type: DataTypes.STRING, allowNull: false},
     password: {type: DataTypes.STRING, allowNull: false},
     role: {type: DataTypes.STRING, defaultValue: "USER"},
     isActivated: {type: DataTypes.BOOLEAN, defaultValue: false},
