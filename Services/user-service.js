f71d43a2e409dafceafbcc314f3607a1f6a02732
Radmir Musabaev <musabaevradmir@gmail.com>
2023-10-02 17:04:47 +0400

    функция логина настроена

--- Services/user-service.js
+++ Services/user-service.js
@@ -37,6 +37,27 @@ class UserService {
         user.isActivated = true
         user.save()
     }
+
+    async login(email, password) {
+        const user = await UserModel.findOne({where: {email}})
+        if (!user) {
+            throw ApiError.BadRequest("User with this email doesn't exist")
+        }
+
+        const isPassEquals = await bcrypt.compare(password, user.password)
+        if (!isPassEquals) {
+            throw ApiError.BadRequest("Uncorrect password")
+        }
+
+        const userDto = new UserDto(user)
+        const tokens = tokenService.generateTokens({...userDto})
+
+        await tokenService.saveToken(userDto.id, tokens.refreshToken);
+        return {
+            ...tokens,
+            user: userDto
+        }
+    }
 }
 
 module.exports = new UserService()
