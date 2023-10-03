41b9fed77898c620078a9a1e20320218b3816916
Radmir Musabaev <musabaevradmir@gmail.com>
2023-10-03 16:53:26 +0400

    Добавил авто модель, бренд модель, класс модель

--- models/models.js
+++ models/models.js
@@ -18,7 +18,38 @@ const TokenModel = sequelize.define('token', {
 UserModel.hasOne(TokenModel)
 TokenModel.hasOne(UserModel)
 
+const CarModel = sequelize.define('car', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    model: {type: DataTypes.STRING, allowNull: false},
+    price: {type: DataTypes.INTEGER, allowNull: false},
+    mileage: {type: DataTypes.INTEGER, allowNull: false},
+    lastMileageOnTS: {type: DataTypes.INTEGER, defaultValue: 0},
+    fuelConsumption: {type: DataTypes.DOUBLE, allowNull: false},
+    //0 - rentalParking, 1 - maintenanceParking
+    technicalCondition: {type: DataTypes.INTEGER, allowNull: false, defaultValue: 0},
+    img: {type: DataTypes.STRING, allowNull: false}
+})
+
+const BrandModel = sequelize.define('brand', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    name: {type: DataTypes.STRING, allowNull: false}
+})
+
+const ClassModel = sequelize.define('class', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    name: {type: DataTypes.STRING, allowNull: false}
+})
+
+BrandModel.hasMany(CarModel)
+CarModel.belongsTo(BrandModel)
+
+ClassModel.hasMany(CarModel)
+CarModel.belongsTo(ClassModel)
+
 module.exports = {
     UserModel, 
-    TokenModel
+    TokenModel,
+    CarModel,
+    BrandModel,
+    ClassModel
 }
