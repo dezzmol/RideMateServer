d30dea58c9070a6c1c411823b33f0d3a4eabcc68
Radmir Musabaev <musabaevradmir@gmail.com>
2023-10-03 19:26:00 +0400

    refresh эндпоинт настроен

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -43,7 +43,10 @@ class UserController {
 
     async refresh(req, res, next) {
         try {
-            return res.json("adada")
+            const {refreshToken} = req.cookies
+            const userData = await userService.refresh(refreshToken)
+            res.cookie("refreshToken", userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true})
+            return res.json(userData)
         } catch (e) {
             next(e)
         }
