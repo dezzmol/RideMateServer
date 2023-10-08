cdba23e9353140ff1ff6f9d0d705f0231d63d845
Radmir Musabaev <musabaevradmir@gmail.com>
2023-10-08 16:51:39 +0400

    добавил функцию создания автомобиля, получения всех автомобилей и получение одного автомобиля по id. Подключил функции к эндпоинтам

---
+++ Services/brand-services.js
@@ -0,0 +1,46 @@
+const {BrandModel} = require("../models/models");
+const ApiError = require("../exceptions/api-error");
+
+class BrandServices {
+    async create(name) {
+        if (!name) {
+            throw ApiError.BadRequest("Name field is empty")
+        }
+
+        const isExist = await BrandModel.findOne({where: {name: name}})
+
+        if (isExist) {
+            throw ApiError.BadRequest("Brand with this name already exist")
+        }
+
+        const brand = await BrandModel.create({name: name})
+
+        return brand
+    }
+
+    async getAll() {
+        const brands = await BrandModel.findAndCountAll()
+
+        if (!brands) {
+            throw ApiError.BadRequest("There are no brands")
+        }
+
+        return brands
+    }
+
+    async getOne(brandId) {
+        if (!brandId) {
+            throw ApiError.BadRequest("Id field is empty")
+        }
+
+        const brand = await BrandModel.findOne({where: {id: brandId}})
+
+        if (!brand) {
+            throw ApiError.BadRequest("Brand with this id doesn't exist")
+        }
+
+        return brand
+    }
+}
+
+module.exports = new BrandServices()
