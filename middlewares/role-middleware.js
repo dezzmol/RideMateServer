a99705d8c9e3e9bdcdc8712dfba191184bfef21a
Radmir Musabaev <musabaevradmir@gmail.com>
2023-10-12 16:21:12 +0400

    Добавил middleware для проверки роли

---
+++ middlewares/role-middleware.js
@@ -0,0 +1,31 @@
+const ApiError = require("../exceptions/api-error");
+const TokenService = require("../services/token-service");
+
+module.exports = function(role) {
+    return async function(req, res, next) {
+        try {
+            const token = req.cookies
+
+            const refreshToken = await TokenService.findToken(token)
+
+            if (!refreshToken) {
+                return next(ApiError.UnauthorizedError());
+            }
+
+            const userData = TokenService.validateRefreshToken(token)
+
+            if (!userData) {
+                return next(ApiError.UnauthorizedError())
+            }
+
+            if (userData.role !== role) {
+                return next(ApiError.NoAccess())
+            }
+
+            req.user = userData;
+            next();
+        } catch (e) {
+            return next(ApiError.UnauthorizedError());
+        }
+    }
+}
