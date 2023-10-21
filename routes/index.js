e340c8cbc48c54f74c33c9c3d56566edab9f34ee
Arsen Bazarbayev <zerxshii@gmail.com>
2023-10-21 12:25:39 +0600

    Создал отдельный роутер для carSchedules

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,18 @@
-const Router = require('express')
+const Router = require("express")
 const router = Router()
 
-const userRouter = require('./userRouter')
+const userRouter = require("./userRouter")
 const cartRouter = require("./carRouter")
 const brandRouter = require("./brandRouter")
 const classRouter = require("./classRouter")
 const parkingRouter = require("./parkingRouter")
+const scheduleRouter = require("./scheduleRouter")
 
 router.use("/user", userRouter)
 router.use("/cars", cartRouter)
 router.use("/brand", brandRouter)
 router.use("/class", classRouter)
 router.use("/parking", parkingRouter)
+router.use("/schedule", scheduleRouter)
 
 module.exports = router
