efc424b94a01b94f1e9be086ffe1c8331bebd083
Arsen Bazarbayev <zerxshii@gmail.com>
2023-10-24 15:06:08 +0600

    Создана функция для получения всей истории аренды определенного пользователя

--- controllers/history-controller.js
+++ controllers/history-controller.js
@@ -1,7 +1,16 @@
 const HistoryService = require("../Services/history-service")
 
 class HistoryController {
-    async getAll(req, res, next) {}
+    async getAll(req, res, next) {
+        try {
+            const userId = req.params.userId
+            const history = await HistoryService.getAll(userId)
+
+            return res.json(history)
+        } catch (e) {
+            next(e)
+        }
+    }
 }
 
 module.exports = new HistoryController()
--- routes/historyRouter.js
+++ routes/historyRouter.js
@@ -2,6 +2,6 @@ const Router = require("express")
 const router = Router()
 const HistoryController = require("../controllers/history-controller")
 
-router.get("/", HistoryController.getAll)
+router.get("/:userId", HistoryController.getAll)
 
 module.exports = router
