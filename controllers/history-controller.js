8db0746acc3253c7f117b32ce8a678f18dc6b0a9
Arsen Bazarbayev <zerxshii@gmail.com>
2023-11-13 21:20:26 +0600

    Изменил возвращаемое значение с сервера на клиент при отмене аренды

--- controllers/history-controller.js
+++ controllers/history-controller.js
@@ -17,10 +17,10 @@ class HistoryController {
         try {
             const { carId, historyId, datesToRemove } = req.body
 
-            const history = await HistoryService.cancelRental(historyId)
+            await HistoryService.cancelRental(historyId)
             await ScheduleService.deleteBookingDates(carId, datesToRemove)
 
-            return res.json(history)
+            return res.json({ message: "The rental was successfully canceled" })
         } catch (e) {
             next(e)
         }
