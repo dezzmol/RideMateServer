81aff3425eef305df963e46082408b2a5b69c477
Arsen Bazarbayev <zerxshii@gmail.com>
2023-11-20 22:44:54 +0600

    Создан функционал для удаления бренда

--- controllers/brand-controller.js
+++ controllers/brand-controller.js
@@ -1,7 +1,7 @@
 const BrandServices = require("../services/brand-services")
 
 class BrandController {
-    async create(req, res, next){
+    async create(req, res, next) {
         try {
             const name = req.body.name
             const brand = await BrandServices.create(name)
@@ -21,12 +21,30 @@ class BrandController {
         }
     }
 
-    async getOne(req, res, next){
+    async getOne(req, res, next) {
         try {
             const brandId = req.params.id
             const brand = await BrandServices.getOne(brandId)
 
             res.json(brand)
+        } catch (e) {
+            next(e)
+        }
+    }
+
+    async delete(req, res, next) {
+        try {
+            const { brandId } = req.body
+
+            const deletionRes = await BrandServices.delete(brandId)
+
+            if (deletionRes === 1) {
+                return res.json(
+                    `The brand with ID ${brandId} was deleted successfully`
+                )
+            }
+
+            return res.json("Something went wrong")
         } catch (e) {
             next(e)
         }
