33f41303f0f7b7d5009f56267468adfec25d81b5
Arsen Bazarbayev <zerxshii@gmail.com>
2023-11-20 22:44:44 +0600

    Создан роут для удаления бренда

--- routes/brandRouter.js
+++ routes/brandRouter.js
@@ -5,5 +5,6 @@ const BrandController = require("../controllers/brand-controller")
 router.get("/", BrandController.getAll)
 router.get("/:id", BrandController.getOne)
 router.post("/", BrandController.create)
+router.delete("/", BrandController.delete)
 
 module.exports = router
