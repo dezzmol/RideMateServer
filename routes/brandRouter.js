9a21dc1221c111688033d71044d73962b20ab52f
Arsen Bazarbayev <zerxshii@gmail.com>
2023-11-30 17:17:54 +0600

    Добавил проверку на роль при создании авто, бренда, класса. И при удалении/восстановлении авто

--- routes/brandRouter.js
+++ routes/brandRouter.js
@@ -1,9 +1,10 @@
 const Router = require("express")
 const router = Router()
 const BrandController = require("../controllers/brand-controller")
+const roleMiddleware = require("../middlewares/role-middleware")
 
 router.get("/", BrandController.getAll)
 router.get("/:id", BrandController.getOne)
-router.post("/", BrandController.create)
+router.post("/", roleMiddleware("ADMIN"), BrandController.create)
 
 module.exports = router
--- routes/classRouter.js
+++ routes/classRouter.js
@@ -1,9 +1,10 @@
 const Router = require("express")
 const router = Router()
 const ClassController = require("../controllers/class-controller")
+const roleMiddleware = require("../middlewares/role-middleware")
 
 router.get("/", ClassController.getAll)
 router.get("/:id", ClassController.getOne)
-router.post("/", ClassController.create)
+router.post("/", roleMiddleware("ADMIN"), ClassController.create)
 
 module.exports = router
